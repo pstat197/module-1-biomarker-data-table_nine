8a5bdfa4e03dcb568ae12b5c9cfef95b47850981
aartigaraye <aarti.garaye@gmail.com>
2025-10-28 18:42:22 -0700

    initial commit

---
+++ scripts/aartigaraye_module1.R
@@ -0,0 +1,5 @@
+# repeat the analysis but carry out the entire selection procedure on a training partition -- 
+# in other words, set aside some testing data at the very beginning and don't use it until you are 
+# evaluating accuracy at the very end
+
+# Necessary packages
